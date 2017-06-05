05c7b4723c1a482cb11e7610df1e98df6bd74e86
Andrey Rogov <rogovandrey@eltechs.com>
2017-06-05 23:51:54 +0300

    Attempt2

--- Android.mk
+++ Android.mk
@@ -3,10 +3,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := ubt
 
-$(shell mkdir -p $(TARGET_OUT)/system/vendor/armv7-guest/ )
-$(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge `pwd`/$(TARGET_OUT)/vendor/lib64/libubt_a32a64_bridge.so )
-$(shell cp -ra $(LOCAL_PATH)/armv7-guest `pwd`/$(TARGET_OUT)/vendor/ )
-$(shell chmod 0755 `pwd`/$(TARGET_OUT)/system/vendor/armv7-guest/mediator )
+$(shell mkdir -p `realpath $(TARGET_OUT_VENDOR)`/armv7-guest )
+$(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge `realpath $(TARGET_OUT_VENDOR)/lib64`/libubt_a32a64_bridge.so )
+$(shell cp -ra $(LOCAL_PATH)/armv7-guest `realpath $(TARGET_OUT_VENDOR)`/ )
+$(shell chmod 0755 `realpath $(TARGET_OUT_VENDOR)`/armv7-guest/mediator )
 
 
 
