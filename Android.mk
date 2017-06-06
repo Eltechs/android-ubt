5de637468f343902e6e134ce2514113729fb7ac1
Andrey Rogov <rogovandrey@eltechs.com>
2017-06-06 15:23:48 +0300

    Revert "Attempt 4"
    This reverts commit 6c0192632cc66584415cc52245f01146fbb0320d.

--- Android.mk
+++ Android.mk
@@ -7,8 +7,9 @@ $(shell mkdir -p `realpath $(TARGET_OUT_VENDOR)`/lib64 )
 $(shell mkdir -p `realpath $(TARGET_OUT_VENDOR)`/armv7-guest )
 $(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge `realpath $(TARGET_OUT_VENDOR)/lib64`/libubt_a32a64_bridge.so )
 $(shell cp -ra $(LOCAL_PATH)/armv7-guest `realpath $(TARGET_OUT_VENDOR)`/ )
+$(shell chmod 0755 `realpath $(TARGET_OUT_VENDOR)`/armv7-guest/mediator )
 
-LOCAL_POST_INSTALL_CMD := chmod 0755 `realpath $(TARGET_OUT_VENDOR)`/armv7-guest/mediator
+
 
 
 
