e90e58bddc030ce8cc7874aa23263bd610ee7bb7
Andrey Rogov <rogovandrey@eltechs.com>
2017-06-08 18:27:43 +0300

    no realpath

--- Android.mk
+++ Android.mk
@@ -3,11 +3,11 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := ubt
 
-$(shell mkdir -p `realpath $(TARGET_OUT_VENDOR)`/lib64 )
-$(shell mkdir -p `realpath $(TARGET_OUT_VENDOR)`/armv7-guest )
-$(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge `realpath $(TARGET_OUT_VENDOR)/lib64`/libubt_a32a64_bridge.so )
-$(shell cp -ra $(LOCAL_PATH)/armv7-guest `realpath $(TARGET_OUT_VENDOR)`/ )
-$(shell chmod 0755 `realpath $(TARGET_OUT_VENDOR)`/armv7-guest/mediator )
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/lib64 )
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/armv7-guest )
+$(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge $(TARGET_OUT_VENDOR)/lib64/libubt_a32a64_bridge.so )
+$(shell cp -ra $(LOCAL_PATH)/armv7-guest $(TARGET_OUT_VENDOR)/ )
+$(shell chmod 0755 $(TARGET_OUT_VENDOR)/armv7-guest/mediator )
 
 
 
