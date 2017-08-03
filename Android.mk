f07a63dd5dcf52ee7d5729ef7946bf28fadfe527
Andrey Rogov <rogovandrey@eltechs.com>
2017-08-03 22:27:53 +0300

    fix path

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ $(shell mkdir -p $(TARGET_OUT_VENDOR)/lib64 )
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/armv7-guest )
 $(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge $(TARGET_OUT_VENDOR)/lib64/libubt_a32a64_bridge.so )
 $(shell cp -ra $(LOCAL_PATH)/armv7-guest $(TARGET_OUT_VENDOR)/ )
-$(shell chmod 0755 $(TARGET_OUT_VENDOR)/armv7-guest/mediator )
+$(shell chmod 0755 $(TARGET_OUT_VENDOR)/armv7-guest/bin/mediator )
 
 
 
