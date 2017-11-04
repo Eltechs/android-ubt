f32d10823ddd70bbb6db6eb0385ce5e4e2c80523
Ubuntu <ubuntu@ip-172-31-5-122.eu-central-1.compute.internal>
2017-11-04 01:17:35 +0000

    New mediator and ubthc

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE := ubt
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/lib64 )
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/armv7-guest )
 $(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge $(TARGET_OUT_VENDOR)/lib64/libubt_a32a64_bridge.so )
+$(shell cp $(LOCAL_PATH)/libubthc.so $(TARGET_OUT_VENDOR)/lib64/libubthc.so )
 $(shell cp -ra $(LOCAL_PATH)/armv7-guest $(TARGET_OUT_VENDOR)/ )
 $(shell chmod 0755 $(TARGET_OUT_VENDOR)/armv7-guest/system/bin/mediator )
 
