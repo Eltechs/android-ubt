8e6b3acda35b7704f6e8a3eed8c8b30c2794e7f7
Your Name <you@example.com>
2018-01-11 18:14:29 +0300

    Android 7.1.1_r22

--- Android.mk
+++ Android.mk
@@ -5,12 +5,8 @@ LOCAL_MODULE := ubt
 
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/lib64 )
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/armv7-guest )
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/bin )
 $(shell cp $(LOCAL_PATH)/ubt_a32a64_bridge $(TARGET_OUT_VENDOR)/lib64/libubt_a32a64_bridge.so )
 $(shell cp $(LOCAL_PATH)/libubthc.so $(TARGET_OUT_VENDOR)/lib64/libubthc.so )
 $(shell cp -ra $(LOCAL_PATH)/armv7-guest $(TARGET_OUT_VENDOR)/ )
-$(shell chmod 0755 $(TARGET_OUT_VENDOR)/armv7-guest/system/bin/mediator )
-
-
-
-
-
+$(shell cp $(LOCAL_PATH)/mediator $(TARGET_OUT_VENDOR)/bin/ )
